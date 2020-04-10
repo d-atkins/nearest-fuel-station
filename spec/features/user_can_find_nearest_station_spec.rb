93b59dca217bc97a4e61862bfef58b535db3840f
David Atkins <atki1080@gmail.com>
2020-04-10 10:27:56 -0600

    Add assertions for map info

--- spec/features/user_can_find_nearest_station_spec.rb
+++ spec/features/user_can_find_nearest_station_spec.rb
@@ -7,30 +7,18 @@ describe "user can visit the welcome page" do
     click_button "Find Nearest Station"
 
     expect(current_path).to eq('/search')
+
     within('#station') do
       expect(first('.name').text).to_not be_empty
       expect(first('.address').text).to_not be_empty
       expect(first('.fuel-type').text).to_not be_empty
       expect(first('.access-times').text).to_not be_empty
     end
+
+    within('#map-info') do
+      expect(first('.distance').text).to_not be_empty
+      expect(first('.travel-time').text).to_not be_empty
+      expect(first('.directions').text).to_not be_empty
+    end
   end
 end
-
-# As a user
-# When I visit "/"
-# And I select "Turing" form the start location drop down (Note: Use the existing search form)
-# And I click "Find Nearest Station"
-# Then I should be on page "/search"
-# Then I should see the closest electric fuel station to me.
-#
-# For that station I should see
-# - Name
-# - Address
-# - Fuel Type
-# - Access Times
-#
-# I should also see:
-# - the distance of the nearest station (should be 0.1 miles)
-# - the travel time from Turing to that fuel station (should be 1 min)
-# - The direction instructions to get to that fuel station
-#   "Turn left onto Lawrence St Destination will be on the left"
