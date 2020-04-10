ae15da9c504116a6f56436aebc0a90393637f948
David Atkins <atki1080@gmail.com>
2020-04-10 10:28:54 -0600

    Add map methods

--- app/facades/station_facade.rb
+++ app/facades/station_facade.rb
@@ -7,4 +7,24 @@ class StationFacade
     station_data = NrelService.nearest_station(@location)
     @station ||= Station.new(station_data)
   end
+
+  def distance
+    map_json[:distance][:text]
+  end
+
+  def travel_time
+    map_json[:duration][:text]
+  end
+
+  def directions
+    map_json[:steps].map do |direction_data|
+      direction_data[:html_instructions]
+    end
+  end
+
+  private
+
+    def map_json
+      @map_data ||= MapService.directions(@location, @station.address)
+    end
 end
